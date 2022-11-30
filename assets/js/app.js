4b8fd6b68f6478c3ab0cbf780048dc58f18b6f35
Alexandr Kurakevich <homewolf777@gmail.com>
2022-11-30 20:25:19 +0300

    My changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,75 +1,69 @@
+console.log("Hi");
 
-$(function() {
-
+$(function () {
   var header = $("#header"),
-      introH = $("#intro").innerHeight();
-      scrollOffset = $(window).scrollTop();
+    introH = $("#intro").innerHeight();
+  scrollOffset = $(window).scrollTop();
 
-    // Fixed Header
+  // Fixed Header
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+
     checkScroll(scrollOffset);
+  });
 
-    $(window).on("scroll", function() {
-        scrollOffset = $(this).scrollTop();
-
-        checkScroll(scrollOffset);
-    });
-
-    function checkScroll(scrollOffset) {
-        if( scrollOffset >= introH ) {
-            header.addClass("fixed"); 
-        } else {
-            header.removeClass("fixed");
-        }
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= introH) {
+      header.addClass("fixed");
+    } else {
+      header.removeClass("fixed");
     }
+  }
 
+  // Smooth scroll
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
 
-    // Smooth scroll
-    $("[data-scroll]").on ("click", function(event) {
-        event.preventDefault();
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
 
-        var $this = $(this),
-            blockId = $this.data('scroll'),
-            blockOffset = $(blockId).offset().top;
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
 
-        $("#nav a").removeClass("active");    
-        $this.addClass("active");
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+  });
 
-        $("html, body").animate({
-            scrollTop: blockOffset
-        },500);
-    }); 
+  // Menu nav toggle
+  $("#nav_toggle").on("click", function (event) {
+    event.preventDefault();
 
+    $(this).toggleClass("active");
+    $("#nav").toggleClass("active");
+  });
 
-    // Menu nav toggle
-    $("#nav_toggle").on("click", function(event) {
-        event.preventDefault();
+  // Collapse
+  $("[data-collapse]").on("click", function (event) {
+    event.preventDefault();
 
-        $(this).toggleClass("active");
-        $("#nav").toggleClass("active");
-    });
-
-
-    // Collapse
-    $("[data-collapse]").on("click", function(event) {
-        event.preventDefault();
-
-        var $this = $(this),
-            blockId = $this.data('collapse');
+    var $this = $(this),
+      blockId = $this.data("collapse");
 
     $this.toggleClass("active");
-    
-});
+  });
 
-
-    // Slider
-    $("[data-slider]").slick({
-        infinity: true,
-        fade: false,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    });
-
-
-
-
+  // Slider
+  $("[data-slider]").slick({
+    infinity: true,
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
 });
